69942ce9129bf908191c1c19e56d5ead5276d8c4
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-01-09 22:51:43 +0800

    class update; refactor everything into class and functions

---
+++ helper.py
@@ -0,0 +1,18 @@
+# Helper file associated with runner.py
+from pygame import font, init
+
+# Define all RGB colors for various components here
+black = (0, 0, 0)
+white = (255, 255, 255)
+tile_color = (0, 160, 0)
+tile_border_color = (110, 38, 14)
+board_color = (50, 50, 50)
+moves_color = (100, 100, 100)
+
+# Define all fonts used here
+init()
+OPEN_SANS = "OpenSans-Regular.ttf"
+smallFont = font.Font(OPEN_SANS, 20)
+hugeFont = font.Font(OPEN_SANS, 40)
+titleFont = font.Font(OPEN_SANS, 50)
+buttonFont = font.Font(OPEN_SANS, 30)
--- runner.py
+++ runner.py
@@ -2,150 +2,138 @@ import pygame
 import sys
 import math
 
-from pygame import surface
-
 from othello import Othello
+from helper import *
 
-# Global variable for size of the board
-HEIGHT = 8
-WIDTH = 8
 
-# Define RGB values of different colors for various components here
-black = (0, 0, 0)
-white = (255, 255, 255)
-tile_color = (0, 160, 0)
-tile_border_color = (110, 38, 14)
-board_color = (50, 50, 50)
-moves_color = (100, 100, 100)
-
-# Initialize game with 9:16 resizable aspect ratio
-pygame.init()
-pygame.display.set_caption("Othello")
-size = width, height = 800, 450
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-
-# Define various fonts here
-OPEN_SANS = "OpenSans-Regular.ttf"
-smallFont = pygame.font.Font(OPEN_SANS, 20)
-hugeFont = pygame.font.Font(OPEN_SANS, 40)
-
-titleFont = pygame.font.Font(OPEN_SANS, 50)
-buttonFont = pygame.font.Font(OPEN_SANS, 30)
-
-# Compute board size
-board_padding = 30
-board_width = ((9 / 16) * width) - (2 * board_padding)
-board_height = height - (2 * board_padding)
-tile_size = int(min(board_width / WIDTH, board_height / HEIGHT))
-board_start = (board_padding, board_padding)
-piece_radius = math.floor(tile_size / 2 - 5)
-
-# States: start (for main menu)
-game_state = "start"
-game_prep = False
-
-ot = Othello(8, 8) #??
-
-def main():
-    while True:
-
-        # Terminate application when the game is quit
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.ext()
-            elif event.type == pygame.VIDEORESIZE:
-                print("dfsf")
-                global screen
-                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-                #screen.blit(surface, screen)
-                # TO-DO: resize feature
+class Game():
+    
+    def __init__(self):
+        self.dim_height = 8
+        self.dim_width = 8
         
+        self.ot = Othello(8, 8)
         
-        if game_state == "start":
-            state_mainmenu()
-            continue
+        self.screen_width = 800
+        self.screen_height = 450
+        self.board_padding = self.screen_height / 15
         
-        if game_state == "play":
-            state_play()
-            continue
+        self.size = width, height = 800, 450
+        self.screen = pygame.display.set_mode(self.size, pygame.RESIZABLE)
+        
+        # Compute board sizes
+        self.board_width = ((9 / 16) * self.screen_width) - (2 * self.board_padding)
+        self.board_height = self.screen_height - (2 * self.board_padding)
+        self.tile_size = int(min(self.board_width / self.dim_width, self.board_height / self.dim_height))
+        self.board_start = (self.board_padding, self.board_padding)
+        self.piece_radius = math.floor(self.tile_size / 2 - 5)
+        
+        self.game_state = "start"
+        self.game_prep = False
+        
+    def run(self):
+        # Initialize game with 9:16 resizable aspect ratio
+        pygame.init()
+        pygame.display.set_caption("Othello")
+
+        
+        while True:
+            # Terminate application when the game is quit
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.ext()
+                elif event.type == pygame.VIDEORESIZE:
+                    print("dfsf")
+                    screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                    #screen.blit(surface, screen)
+                    # TO-DO: resize feature
+
+                if self.game_state == "start":
+                    self.state_mainmenu()
+                    continue
+                
+                if self.game_state == "play":
+                    self.state_play()
+                    continue
+                
+                pygame.display.flip()
+                
+    def state_mainmenu(self):
+        # Display title
+        self.screen.fill(black)
+        title = titleFont.render("Hello Othello", True, white)
+        titleRect = title.get_rect()
+        titleRect.center = (self.screen_width / 2, 50)
+        self.screen.blit(title, titleRect)
+        
+        # Display play button
+        buttonRect = pygame.Rect((self.screen_width / 4), (9 / 16) * self.screen_height, self.screen_width / 2, 50)
+        buttonText = buttonFont.render("Play Game", True, black)
+        buttonTextRect = buttonText.get_rect()
+        buttonTextRect.center = buttonRect.center
+        pygame.draw.rect(self.screen, white, buttonRect)
+        self.screen.blit(buttonText, buttonTextRect)
+        
+        # Change the game_state to "play" if play button is left-clicked
+        left, _, _ = pygame.mouse.get_pressed()
+        if left == 1:
+            mouse = pygame.mouse.get_pos()
+            if buttonRect.collidepoint(mouse):
+                self.game_state = "play"
         
         pygame.display.flip()
 
-
-def state_mainmenu():
-    
-    # Display title
-    screen.fill(black)
-    title = titleFont.render("Hello Othello", True, white)
-    titleRect = title.get_rect()
-    titleRect.center = (width / 2, 50)
-    screen.blit(title, titleRect)
-    
-    # Display play button
-    buttonRect = pygame.Rect((width / 4), (9 / 16) * height, width / 2, 50)
-    buttonText = buttonFont.render("Play Game", True, black)
-    buttonTextRect = buttonText.get_rect()
-    buttonTextRect.center = buttonRect.center
-    pygame.draw.rect(screen, white, buttonRect)
-    screen.blit(buttonText, buttonTextRect)
-    
-    # Change the game_state to "play" if play button is left-clicked
-    left, _, _ = pygame.mouse.get_pressed()
-    if left == 1:
+    def state_play(self):
+        
+        # Initialize game with default settings (first time only)
+        if not self.game_prep:
+            init_white = [(3, 3), (4, 4)]
+            init_black = [(3, 4), (4, 3)]
+            self.ot.set_initial_position(init_white, init_black)
+            self.ot.turn = 2
+            self.game_prep = True
+        
+        # Draw board and all the tiles
+        self.screen.fill(black)
+        tiles = []
+        for i in range(self.dim_height):
+            row_tiles = []
+            for j in range(self.dim_width):
+                rect = pygame.Rect(self.board_start[0] + j * self.tile_size, self.board_start[1] + i * self.tile_size, self.tile_size, self.tile_size)
+                pygame.draw.rect(self.screen, tile_color, rect)
+                pygame.draw.rect(self.screen, board_color, rect, 3)
+                row_tiles.append(rect)
+            tiles.append(row_tiles)
+        
+        # Draw each pieces that are present in the board, including all tiles with possible move
+        moves = self.ot.get_possible_moves()
+        for i in range(self.dim_height):
+            for j in range(self.dim_width):
+                coordinate = (self.board_start[0] + j * self.tile_size + self.tile_size / 2, self.board_start[1] + i * self.tile_size + self.tile_size / 2)
+                if self.ot.board[i][j] != 0:
+                    circ = pygame.draw.circle(self.screen, tile_border_color, coordinate, self.piece_radius + 2)
+                    circ = pygame.draw.circle(self.screen, (white if self.ot.board[i][j] == 1 else black), coordinate, self.piece_radius)
+                if (i, j) in moves:
+                    circ = pygame.draw.circle(self.screen, moves_color, coordinate, self.piece_radius, int(self.piece_radius / 2))
+        
+        # Update changes when a valid tile is clicked
+        left, _, right = pygame.mouse.get_pressed()
         mouse = pygame.mouse.get_pos()
-        if buttonRect.collidepoint(mouse):
-            global game_state
-            game_state = "play"
-    
-    pygame.display.flip()
+        if left == 1:
+            for i in range(self.dim_height):
+                for j in range(self.dim_width):
+                    if tiles[i][j].collidepoint(mouse):
+                        if (i, j) in moves:
+                            self.ot.make_move((i, j))
+                        # ot.terminal_print()
+                        # if ot.check_victory() != 0:
+                        #     break
+        
+        pygame.display.flip()         
+            
 
 
-def state_play():
-    
-    # Initialize game with default settings (first time only)
-    global game_prep
-    if not game_prep:
-        init_white = [(3, 3), (4, 4)]
-        init_black = [(3, 4), (4, 3)]
-        ot.set_initial_position(init_white, init_black)
-        ot.turn = 2
-        game_prep = True
-    
-    # Draw board and all the tiles
-    screen.fill(black)
-    tiles = []
-    for i in range(HEIGHT):
-        row_tiles = []
-        for j in range(WIDTH):
-            rect = pygame.Rect(board_start[0] + j * tile_size,board_start[1] + i * tile_size, tile_size, tile_size)
-            pygame.draw.rect(screen, tile_color, rect)
-            pygame.draw.rect(screen, board_color, rect, 3)
-            row_tiles.append(rect)
-        tiles.append(row_tiles)
-    
-    # Draw each pieces that are present in the board, including all tiles with possible move
-    moves = ot.get_possible_moves()
-    for i in range(HEIGHT):
-        for j in range(WIDTH):
-            coordinate = (board_start[0] + j * tile_size + tile_size / 2, board_start[1] + i * tile_size + tile_size / 2)
-            if ot.board[i][j] != 0:
-                circ = pygame.draw.circle(screen, tile_border_color, coordinate, piece_radius + 2)
-                circ = pygame.draw.circle(screen, (white if ot.board[i][j] == 1 else black), coordinate, piece_radius)
-            if (i, j) in moves:
-                circ = pygame.draw.circle(screen, moves_color, coordinate, piece_radius, int(piece_radius / 2))
-    
-    # Update changes when a valid tile is clicked
-    left, _, right = pygame.mouse.get_pressed()
-    mouse = pygame.mouse.get_pos()
-    if left == 1:
-        for i in range(HEIGHT):
-            for j in range(WIDTH):
-                if tiles[i][j].collidepoint(mouse):
-                    if (i, j) in moves:
-                        ot.make_move((i, j))
-                    # ot.terminal_print()
-                    # if ot.check_victory() != 0:
-                    #     break
-    
-    pygame.display.flip()
-    
+def main():
+    game = Game()
+    game.run()
+
 main()
