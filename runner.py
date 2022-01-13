fba11ab53bce38b2e5e2d430ddbc6ed95408d979
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-01-13 23:31:49 +0800

    initial testing to toggle on/off bgm sound

--- runner.py
+++ runner.py
@@ -69,7 +69,7 @@ class Game():
         self.soundfx_on = True
         
         # Various hover effects
-        self.hover_main = [False for i in range(3)]
+        self.hover_main = [False for i in range(9)]
     
     # Resize components relative to the overall screen width and height
     def resize(self):
@@ -130,8 +130,9 @@ class Game():
     def play_main_bgm(self):
         if not pygame.mixer.music.get_busy():
             pygame.mixer.music.load(BGM_MENU)
-            pygame.mixer.music.set_volume(0.1)
+            pygame.mixer.music.set_volume((0.1 if self.bgm_on == True else 0))
             pygame.mixer.music.play(loops = -1)
+        pygame.mixer.music.set_volume((0.1 if self.bgm_on == True else 0))
     
     # Undo the last move, adjust all othello board configuration to the previous state
     def undo_move(self):
@@ -147,19 +148,23 @@ class Game():
         self.screen.blit(title, titleRect)
         
         # Display buttons (classic mode, custom mode, puzzle mode)
-        button_texts = ["Classic Mode", "Custom Mode", "Puzzle Mode"]
+        button_texts = ["Classic Mode", "Custom Mode", "Puzzle Mode", "", "How To Play", "Leaderboards", "About", "BGM", "SFX"]
         button_dict = {}
-        for i in range(3):
-            buttonRect = pygame.Rect((self.screen_width / 4), ((6 + 2 * i) / 16) * self.screen_height, self.screen_width / 2, self.screen_height / 10)
-            if self.hover_main[i] == True:
-                buttonRect = pygame.Rect((self.screen_width / 4), ((6 + 2 * i) / 16) * self.screen_height, (self.screen_width / 2), (self.screen_height / 10) * 1.05)
+        for i in range(len(button_texts)):
+            if 0 <= i <= 3:
+                buttonRect = pygame.Rect((self.screen_width / 4), ((6 + 2 * i) / 16) * self.screen_height, self.screen_width / 2, self.screen_height / 10)
+                if self.hover_main[i] == True: buttonRect = pygame.Rect((self.screen_width / 4), ((6 + 2 * i) / 16) * self.screen_height, (self.screen_width / 2), (self.screen_height / 10) * 1.05)
+                buttonText = buttonFont.render(button_texts[i], True, main_button_text_color)
+                if self.hover_main[i] == True: buttonText = buttonFont.render(button_texts[i], True, main_button_text_hover_color)
+            elif i == 7 or i == 8:
+                buttonRect = pygame.Rect((self.screen_width / 4), (self.screen_height * (-0.6 + i * 0.1)), self.screen_width / 4, self.screen_height / 20)
+                buttonText = buttonFont.render(button_texts[i], True, main_button_text_color)
             button_dict[i] = buttonRect
-            buttonText = buttonFont.render(button_texts[i], True, main_button_text_color)
-            if self.hover_main[i] == True: buttonText = buttonFont.render(button_texts[i], True, main_button_text_hover_color)
             buttonTextRect = buttonText.get_rect()
             buttonTextRect.center = buttonRect.center
-            if self.hover_main[i] == True: pygame.draw.rect(self.screen, main_button_hover_color, buttonRect)
-            else: pygame.draw.rect(self.screen, main_button_color, buttonRect)
+            if 0 <= i <= 2 or i == 7 or i == 8:
+                if self.hover_main[i] == True: pygame.draw.rect(self.screen, main_button_hover_color, buttonRect)
+                else: pygame.draw.rect(self.screen, main_button_color, buttonRect)
             self.screen.blit(buttonText, buttonTextRect)
         
         # Change state when respective buttons are clicked, add hover effects
@@ -170,7 +175,8 @@ class Game():
             for i in range(3):
                 if button_dict[i].collidepoint(mouse): self.game_menu = states[i]
                 time.sleep(0.1)
-        self.hover_main = [False for i in range(3)]
+            if button_dict[7].collidepoint(mouse): self.bgm_on = (False if self.bgm_on == True else True)
+        self.hover_main = [False for i in range(9)]
         for i in range(3):
             if button_dict[i].collidepoint(mouse): self.hover_main[i] = True
         
