7f5efdf5826240ecd60ef8f4b321c1ff4ce882b6
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-01-18 20:39:53 +0800

    basic regression file structure created

--- trainer.py
+++ trainer.py
@@ -1,40 +1,51 @@
-""" 
+"""
 Learning File
 """
 
 import sklearn
+import sklearn.model_selection
+import sklearn.metrics
 import sys
+import csv
+from sklearn import linear_model
+
 
 def main():
-
-    # Check command-line arguments
+    # Ensure correct usage, load training data, split into training and test sets
     if len(sys.argv) != 2:
         sys.exit("Usage: python trainer.py <data>")
-
-    # Load data from spreadsheet and split into train and test sets
-    evidence, labels = sklearn.load_data(sys.argv[1])
-    x_train, x_test, y_train, y_test = sklearn.train_test_split(
-        evidence, labels, test_size = 0.2
+    parameters, real_values = load_data(sys.argv[1])
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        parameters, real_values, test_size = 0.01
     )
 
-    # Train model and make predictions
+    # Train model, then displaying some analytical data based on the learning
     model = train_model(x_train, y_train)
-    predictions = model.predict(x_test)
-    
+    analyze_model(model)
 
+
+# Load the necessary data from the csv file, divide into a list of all parameters value and another vector list of all expected values
 def load_data(dir):
-    raise NotImplementedError
+    with open(dir) as file:
+        next(file)
+        reader = csv.reader(file)
+        parameters, real_values = [], []
+        for row in reader:
+            parameters.append([int(row[0]), int(row[1])])
+            real_values.append(int(row[2]))
+    return (parameters, real_values)
 
 
+# Using linear regression to train the model, given a list of parameters (x) and expected value (y) from the training data
 def train_model(x_train, y_train):
-    raise NotImplementedError
-    # regressor = sklearn.LinearRegression()
-    # regressor.fit(x_train, y_train)
-    # pass
+    regressor = linear_model.LinearRegression()
+    regressor.fit(x_train, y_train)
+    return regressor
 
 
-def retrieve_coef():
-    raise NotImplementedError
+# Print out the coefficients, make further analysis of the data... #TODO
+def analyze_model(model):
+    print("Coefficients: \n", model.coef_)
 
 
 if __name__ == "__main__":
