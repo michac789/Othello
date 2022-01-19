70cf44537094a34fafcc2628d18882755193831e
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-01-19 23:31:22 +0800

    added logistic regression method in sample_trainer

--- sample_trainer.py
+++ sample_trainer.py
@@ -8,27 +8,31 @@ Please note that this is only using some arbitrary data, and not used to train t
 
 import sklearn
 import sklearn.model_selection
+import sklearn.linear_model
 import sklearn.metrics
 import csv
 import os
-from sklearn import linear_model
 
 
-# Load training data, split into training and test sets, train and analyze model
+# Load training data, split into training and test sets, train and analyze model, getting result from new data
 def main():
     # (1) Linear Regression:
-    parameters, real_values = load_data(os.path.join("learning_data", "sample1.csv"), 1)
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-        parameters, real_values, test_size = 0.2
-    )
-    model = train_model_linreg(x_train, y_train)
-    predictions = model.predict(x_test)
-    analyze_model_linreg(model, y_test, predictions)
+    x_train1, x_test1, y_train1, y_test1 = load_data(os.path.join("learning_data", "sample1.csv"), 1)
+    model1 = train_model_linreg(x_train1, y_train1)
+    predictions1 = model1.predict(x_test1)
+    analyze_model_linreg(model1, y_test1, predictions1)
     
     # (2) Logistic Regression
+    x_train2, x_test2, y_train2, y_test2 = load_data(os.path.join("learning_data", "sample2.csv"), 2)
+    model2 = train_model_logreg(x_train2, y_train2)
+    predictions2 = model2.predict(x_test2)
+    analyze_model_logreg(model2, y_test2, predictions2)
+    
+    # Using the above models to predict based on some evidences
+    # TODO
 
 
-# Load the necessary data from the csv file, divide into a list of all parameters value and another vector list of all expected values
+# Load the necessary data from the csv file, divide into training and test parameters and outcome, return those values
 def load_data(dir, type):
     with open(dir) as file:
         next(file)
@@ -37,31 +41,54 @@ def load_data(dir, type):
         for row in reader:
             parameters.append([int(row[0]), int(row[1]), int(row[2])])
             if type == 1: real_values.append(int(row[3]))
-            elif type == 2: real_values.append(row[2])
-    return (parameters, real_values)
+            elif type == 2: real_values.append(row[3])
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(parameters, real_values, test_size = 0.2)
+    return (x_train, x_test, y_train, y_test)
 
 
 # Using linear regression to train a model
 def train_model_linreg(x_train, y_train):
-    regressor = linear_model.LinearRegression()
+    regressor = sklearn.linear_model.LinearRegression()
     regressor.fit(x_train, y_train)
     return regressor
 
 
 # Using logistic regression to train a model
 def train_model_logreg(x_train, y_train):
-    raise NotImplementedError
+    logreg = sklearn.linear_model.LogisticRegression()
+    logreg.fit(x_train, y_train)
+    return logreg
 
 
 # Print out the coefficients gained and mean squared error from the test data (linear regression)
 def analyze_model_linreg(model, y_test, predictions):
+    print("Linear Regression")
     print("Coefficients gained: \n", model.coef_)
-    print("Mean squared error: %.2f" % sklearn.metrics.mean_squared_error(y_test, predictions))
+    print("Mean squared error: %.2f\n" % sklearn.metrics.mean_squared_error(y_test, predictions))
 
 
-# ??? (logistic regression)
+# Print out the true positive (win) rate and true negative (lose) rate (logistic regression)
 def analyze_model_logreg(model, y_test, predictions):
-    raise NotImplementedError
+    positive_labels_count, negative_labels_count, positive_labels_accurate, negative_labels_accurate = 0, 0, 0, 0
+    for label, prediction in zip(y_test, predictions):
+        if label == "win":
+            positive_labels_count += 1
+            if label == prediction: positive_labels_accurate += 1
+        elif label == "lose":
+            negative_labels_count += 1
+            if label == prediction: negative_labels_accurate += 1
+    print("Logistic Regression")
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    if positive_labels_count != 0:
+        sensitivity = positive_labels_accurate / positive_labels_count # True positive rate (percentage when 'win' is guessed correctly)
+        print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    else: print("True Positive Rate: Undetermined (not enough test data)")
+    if negative_labels_count != 0:
+        specificity = negative_labels_accurate / negative_labels_count # True negative rate (percentage when 'lose' is guessed correctly)
+        print(f"True Negative Rate: {100 * specificity:.2f}%")
+    else: print("True Negative Rate: Undetermined (not enough test data)")
+    print("")
 
 
 if __name__ == "__main__":
